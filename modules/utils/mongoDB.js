2b6193ca20ace13e0365aff9eaea9e502b2ed078
GendSmith <dzt2727@gmail.com>
2018-02-06 15:58:19 +0800

    MongoDB修改成本地开发的URL

--- modules/utils/mongoDB.js
+++ modules/utils/mongoDB.js
@@ -1,8 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const config = require('../constants/config');
-
 const mongoConf = config.databaseOptions.mongodb;
-const url = 'mongodb://' + mongoConf.user + ':' + mongoConf.password + '@' + mongoConf.host + ':' + mongoConf.port + '/' + mongoConf.database;
+
+//非本地开发
+//const url = 'mongodb://' + mongoConf.user + ':' + mongoConf.password + '@' + mongoConf.host + ':' + mongoConf.port + '/' + mongoConf.database;
+
+//本地开发
+const url = 'mongodb://' + mongoConf.host + ':' + mongoConf.port + '/'
+
 
 let connect = function (func) {
     MongoClient.connect(url, function (err, db) {
